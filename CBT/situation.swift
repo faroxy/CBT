97b680da7f1c6e71d4962024fbee7df8dcc4f85e
faroxy <t05m047b@yahoo.co.jp>
2016-04-18 21:25:30 +0900

    完了ボタン設置

--- CBT/situation.swift
+++ CBT/situation.swift
@@ -7,21 +7,49 @@
 //
 
 import UIKit
+import QuartzCore
+
 
 class situation: UIViewController {
     
+
+    @IBOutlet weak var situation: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //situation.text = "a\nb\nc\nd"
+        //situation.backgroundColor = UIColor.grayColor()
+        situation.layer.masksToBounds = true
+        situation.layer.cornerRadius = 10.0
+        situation.layer.borderWidth = 1
+        situation.layer.borderColor = UIColor.blueColor().CGColor
+        
+        //完了ボタン
+        let kbToolBar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 320, height: 40))
+        kbToolBar.barStyle = UIBarStyle.Default  // スタイルを設定
+        
+        kbToolBar.sizeToFit()  // 画面幅に合わせてサイズを変更
+        
+        // スペーサー
+        let spacer = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.FlexibleSpace, target: self, action: nil)
+        
+        // 閉じるボタン
+        let commitButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Done, target: self, action: "commitButtonTapped")
+        
+        
+        kbToolBar.items = [spacer, commitButton]
+        
+        
+        situation.inputAccessoryView = kbToolBar
+        //完了ボタン
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func commitButtonTapped (){
+        self.view.endEditing(true)
     }
     
-
     
 }
 
